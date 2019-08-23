c93d6e357f7fce297dcc14ebe1f2964de89d694c
Niloy Niil <blueniil9@gmail.com>
2019-08-23 17:21:26 +0600

    welcome to firebase

---
+++ app/src/main/java/com/johnwick/mymap/MainActivity.java
@@ -0,0 +1,65 @@
+package com.johnwick.mymap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private DatabaseReference mDatabase;
+    private Button btnSave;
+    private Button btnProceed;
+    private EditText editTextLocationName;
+    private EditText editTextLocationLat;
+    private EditText editTextLocationLong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDatabase=FirebaseDatabase.getInstance().getReference().child("Users");
+        editTextLocationName=(EditText)findViewById(R.id.editTextLocationName);
+        editTextLocationLat=(EditText)findViewById(R.id.editTextLocationLat);
+        editTextLocationLong=(EditText)findViewById(R.id.editTextLocationLong);
+        btnSave=(Button)findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+        btnProceed=(Button)findViewById(R.id.btnProceed);
+        btnProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,MapsActivity.class); //Add it later
+                startActivity(i);
+            }
+        });
+    }
+
+    private void saveUserInformation(){
+        String name =editTextLocationName.getText().toString().trim();
+        double latitude= Double.parseDouble(editTextLocationLat.getText().toString().trim());
+        double longitude= Double.parseDouble(editTextLocationLong.getText().toString().trim());
+        UserInformation userInformation=new UserInformation(name,latitude,longitude);
+        mDatabase.child("Users").setValue(userInformation);
+        Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view==btnProceed){
+            finish();
+        }
+        if(view==btnSave){
+            saveUserInformation();
+            editTextLocationName.getText().clear();
+            editTextLocationLat.getText().clear();
+            editTextLocationLong.getText().clear();
+        }
+    }
+}
--- app/src/main/java/com/johnwick/mymap/MapsActivity.java
+++ app/src/main/java/com/johnwick/mymap/MapsActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -17,13 +18,24 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-//    private GoogleMap mMap;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener,GoogleMap.OnMarkerClickListener {
+    private GoogleMap mMap;
+    private ChildEventListener mChildEventListener;
+    private DatabaseReference mUsers;
+    Marker marker;
 
     Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
@@ -34,11 +46,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        ChildEventListener mChildEventListener;
+        mUsers= FirebaseDatabase.getInstance().getReference("Users");
+        mUsers.push().setValue(marker);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        fetchLastLocation();
+
+//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+//        fetchLastLocation();
     }
-
+/*
     private void fetchLastLocation() {
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -71,50 +89,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_maps);
-//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-//
-//
-//    }
-    
-/*    @Override
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Home and move the camera
-        LatLng home = new LatLng(23.8338998,90.415805);
+        LatLng home = new LatLng(23.8338998, 90.415805);
         mMap.addMarker(new MarkerOptions().position(home).title("Marker in Home"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(home));
 
         // Add a marker in Dhaka Regency and move the camera
-        LatLng dhakaregency = new LatLng(23.8294358,90.4193755);
+        LatLng dhakaregency = new LatLng(23.8294358, 90.4193755);
         mMap.addMarker(new MarkerOptions().position(dhakaregency).title("Marker in Dhaka Regency"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(dhakaregency));
 
         // Add a marker in Home and move the camera
-        LatLng lemeridien = new LatLng(23.8364069,90.4174776);
+        LatLng lemeridien = new LatLng(23.8364069, 90.4174776);
         mMap.addMarker(new MarkerOptions().position(lemeridien).title("Marker in Le Méridien Dhaka"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(lemeridien));
-    }*/
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
+
         LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am here.");
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,11));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 3));
         googleMap.addMarker(markerOptions);
     }
 
 
-//    @Override
-    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -124,4 +141,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+}
+*/
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+    mMap=googleMap;
+    googleMap.setOnMarkerClickListener(this);
+    googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    mUsers.addListenerForSingleValueEvent(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            for(DataSnapshot s : dataSnapshot.getChildren()){
+                UserInformation user = s.getValue(UserInformation.class);
+                LatLng location=new LatLng(user.latitude,user.longitude);
+                mMap.addMarker(new MarkerOptions().position(location).title(user.name)).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
 }
